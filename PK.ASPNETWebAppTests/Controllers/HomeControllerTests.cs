29ff5fbfcae681da6e32c2f869fcbc501f2e7fb6
Prasenjit Paul <prasenjit.p@hcl.com>
2020-07-30 08:13:24 +0000

    message you want for your first commit

---
+++ PK.ASPNETWebAppTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PK.ASPNETWebApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace PK.ASPNETWebApp.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            var controller = new HomeController();
+            ActionResult result = controller.Index();
+            Assert.AreEqual(null, controller.ViewBag.Message);
+        }
+
+        [TestMethod()]
+        public void AboutTest()
+        {
+            var controller = new HomeController();
+            ActionResult result = controller.About();
+            Assert.AreEqual("Your application description page.", controller.ViewBag.Message);
+        }
+
+        [TestMethod()]
+        public void ContactTest()
+        {
+            var controller = new HomeController();
+            ActionResult result = controller.Contact();
+            Assert.AreEqual("Your contact page.", controller.ViewBag.Message);
+        }
+    }
+}
